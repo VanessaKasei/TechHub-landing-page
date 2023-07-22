112cddf72f1c9f59fb0a8055d6603d4a7b58b058
VanessaKasei <kaseivanessa@gmail.com>
2023-07-22 23:41:21 +0300

    photo1 and learn more button
    description

---
+++ index.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Add an event listener to detect when the page has fully loaded
+window.addEventListener("load", function() {
+    const slideInText = document.querySelector(".slide-in-text");
+    slideInText.classList.add("slide-in"); // Add the slide-in class to trigger the sliding effect on page load
+});
